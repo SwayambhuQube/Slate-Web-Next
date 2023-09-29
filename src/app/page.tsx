def203f55d9cc8bc52c47ce7d04964429ea2785b
SwayambhuQube <swayambhu.panda@qubecinema.com>
2023-09-29 14:44:33 +0530

    Initial setup
    * Initial setup
    * Sidebar common component added

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Sidebar } from "@/components/common/sidebar/Sidebar";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <Sidebar />
+    </>
+  );
+}
