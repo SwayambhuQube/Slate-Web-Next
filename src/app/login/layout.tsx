2a080edf08a1d09e34a5e6233e999a010fc9c260
SwayambhuQube <swayambhu.panda@qubecinema.com>
2024-01-10 12:06:17 +0530

    Remove unnecessary code and update Sign in button

---
+++ src/app/login/layout.tsx
@@ -0,0 +1,35 @@
+import { SlateLogo } from "@/assets";
+import Image from "next/image";
+
+export default function LoginLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div>
+      <div className="h-screen w-screen flex justify-center items-center bg-[#052f66]">
+        <div className=" min-w-[310px] h-fit max-w-[510px] p-10 mx-10 bg-white rounded-lg">
+          <div>
+            <Image
+              className="my-6"
+              src={SlateLogo}
+              height={32}
+              width={225}
+              alt="no logo"
+            />
+          </div>
+          <div className="my-6 text-center text-[#69788c] text-sm">
+            Qube Slate is a content scheduling platform that acts as a
+            marketplace to allow Advertisers, Media Agencies and Exhibitors to
+            plan campaigns, target content to specific theatres, and schedule
+            content at the theatres for playback.
+          </div>
+          <div className="w-full flex justify-center items-center">
+            {children}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,5 @@
 "use client";
-import { SlateLogo } from "@/assets";
 import { createQueryString } from "@/utils/queryString";
-import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 const QA_CLIENT_ID = "7079f5e5754c4471aa01b09e81675335";
@@ -20,34 +18,11 @@ export default function Login() {
     router.push(url);
   };
   return (
-    <>
-      <div className="h-screen w-screen flex justify-center items-center bg-[#052f66]">
-        <div className=" min-w-[310px] h-fit max-w-[510px] p-10 mx-10 bg-white rounded-lg">
-          <div>
-            <Image
-              className="my-6"
-              src={SlateLogo}
-              height={32}
-              width={225}
-              alt="no logo"
-            />
-          </div>
-          <div className="my-6 text-center text-[#69788c] text-sm">
-            Qube Slate is a content scheduling platform that acts as a
-            marketplace to allow Advertisers, Media Agencies and Exhibitors to
-            plan campaigns, target content to specific theatres, and schedule
-            content at the theatres for playback.
-          </div>
-          <div className="w-full flex justify-center items-center">
-            <button
-              className="min-h-12 min-w-[310px] text-white bg-[#052f66] p-4 rounded-lg mx-2"
-              onClick={login}
-            >
-              Signin
-            </button>
-          </div>
-        </div>
-      </div>
-    </>
+    <button
+      className="min-h-12 min-w-[310px] text-white bg-[#052f66] p-4 rounded-lg mx-2"
+      onClick={login}
+    >
+      Sign in
+    </button>
   );
 }
